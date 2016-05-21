6b992c8d87ee2ce0c794fbda6d904152c70c1ec5
lorenzvth7 <lorenz.vanthillo@outlook.com>
2016-05-22 01:28:13 +0200

    add ghost project

---
+++ config/config.js
@@ -0,0 +1,10 @@
+database: {
+           client: 'mysql',
+           connection: {
+               host     : 'mysql',
+               user     : 'ghost',
+               password : 'password',
+               database : 'ghost',
+               charset  : 'utf8'
+           }
+       },
