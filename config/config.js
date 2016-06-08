7a26872ae8cf4575a8aaf757ba56484279d6ec01
lorenzvth7 <lorenz.vanthillo@outlook.com>
2016-06-08 08:47:29 +0200

    change port + config.js

--- config/config.js
+++ config/config.js
@@ -33,18 +33,33 @@ config = {
         // Change this to your Ghost blog's published URL.
         url: 'http://localhost:2368',
 
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
 
         // #### Database
         // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
-           client: 'mysql',
-           connection: {
-               host     : 'mysql',
-               user     : 'ghost',
-               password : 'password',
-               database : 'ghost',
-               charset  : 'utf8'
-           }
+             client: 'mysql',
+             connection: {
+                 host: 'mysql',
+                 user: 'blog-user',
+                 password: 'supersecret',
+                 database: 'db',
+                 charset: 'utf8'
+             },
+            debug: false
         },
         // #### Server
         // Can be host & port (default), or socket
@@ -59,8 +74,77 @@ config = {
         paths: {
             contentPath: path.join(process.env.GHOST_CONTENT, '/')
         }
-    }
+    },
 
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-test.db')
+            },
+            pool: {
+                afterCreate: function (conn, done) {
+                    conn.run('PRAGMA synchronous=OFF;' +
+                    'PRAGMA journal_mode=MEMORY;' +
+                    'PRAGMA locking_mode=EXCLUSIVE;' +
+                    'BEGIN EXCLUSIVE; COMMIT;', done);
+                }
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing MySQL
+    // Used by Travis - Automated testing run through GitHub
+    'testing-mysql': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing pg
+    // Used by Travis - Automated testing run through GitHub
+    'testing-pg': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    }
 };
 
 module.exports = config;
